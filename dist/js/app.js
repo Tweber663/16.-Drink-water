07b1776998cacd9e2324720528b437330086f7b4
Tweber663 <tweber.1995@hotmail.com>
2024-09-21 13:51:25 +0200

    small cups are filling correctly

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,19 +1,21 @@
 "use strict";
 const cups = document.querySelectorAll('.cup-small');
+const filledCups = document.querySelectorAll('.cup-small.full');
+const countingCups = () => {
+    console.log(filledCups);
+};
 const selectingCups = (e, index) => {
-    console.log(index);
-    console.log(e.target);
     if (!e.target.classList.contains('full')) {
-        console.log('clicked on full');
         for (let i = 0; i < index; i++) {
-            setTimeout(() => {
-                Array.from(cups)[i].classList.add('full');
-            }, 100);
+            Array.from(cups)[i].classList.add('full');
         }
     }
     else {
-        console.log('clicked on Empty');
+        for (let i = 8; index - 1 < i; i--) {
+            Array.from(cups)[i - 1].classList.remove('full');
+        }
     }
+    countingCups();
 };
 Array.from(cups).forEach((smallCup, index) => {
     smallCup.addEventListener('click', (e) => selectingCups(e, index + 1));
--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,24 @@
 const cups = document.querySelectorAll('.cup-small') as NodeListOf<HTMLDivElement>; 
+const filledCups = document.querySelectorAll('.cup-small.full') as NodeListOf<HTMLDivElement>; 
+
+
+
+const countingCups = () => {
+    console.log(filledCups); 
+}
 
 
 const selectingCups = (e: Event, index: number) => {
-    console.log(index)
-    console.log(e.target)
     if (!e.target.classList.contains('full')) {
-        console.log('clicked on full'); 
         for(let i = 0; i < index; i++) {
-            setTimeout(() => {
-                Array.from(cups)[i].classList.add('full'); 
-            },  100);
+            Array.from(cups)[i].classList.add('full'); 
         }
     } else {
-        console.log('clicked on Empty')
+        for(let i = 8; index - 1 < i; i--) {
+            Array.from(cups)[i - 1].classList.remove('full');
+        }
     }
+    countingCups(); 
 }
 
 Array.from(cups).forEach((smallCup, index) => {
