aae2b99111658e3fd6ed752eb631131804d19f2c
Tweber663 <tweber.1995@hotmail.com>
2024-09-21 13:25:00 +0200

    working on JS filling bottles

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,6 +1,21 @@
 "use strict";
 const cups = document.querySelectorAll('.cup-small');
+const selectingCups = (e, index) => {
+    console.log(index);
+    console.log(e.target);
+    if (!e.target.classList.contains('full')) {
+        console.log('clicked on full');
+        for (let i = 0; i < index; i++) {
+            setTimeout(() => {
+                Array.from(cups)[i].classList.add('full');
+            }, 100);
+        }
+    }
+    else {
+        console.log('clicked on Empty');
+    }
+};
 Array.from(cups).forEach((smallCup, index) => {
-    console.log(index + 1);
+    smallCup.addEventListener('click', (e) => selectingCups(e, index + 1));
 });
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,22 @@
 const cups = document.querySelectorAll('.cup-small') as NodeListOf<HTMLDivElement>; 
 
 
+const selectingCups = (e: Event, index: number) => {
+    console.log(index)
+    console.log(e.target)
+    if (!e.target.classList.contains('full')) {
+        console.log('clicked on full'); 
+        for(let i = 0; i < index; i++) {
+            setTimeout(() => {
+                Array.from(cups)[i].classList.add('full'); 
+            },  100);
+        }
+    } else {
+        console.log('clicked on Empty')
+    }
+}
+
 Array.from(cups).forEach((smallCup, index) => {
-    console.log(index + 1)
+    smallCup.addEventListener('click', (e) => selectingCups(e, index + 1)); 
 })
+
